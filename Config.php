4705e6f2b83c8657eefb465d1556cffe0d03e145
Naman Aggarwal <nam.aggarwal@yahoo.com>
2014-03-01 03:21:32 +0800

    config file

--- Config.php
+++ Config.php
@@ -1,8 +1,12 @@
 <?php
 
-class Config{
+class config{
 
-	const DB_NAME = "scrap";
-	const TAB_NAME = "link";
+	const DB_NAME = "ebargain";
+	const LINK_TAB_NAME = "tbllink";
+	const CONTACT_TAB_NAME = "tblcontact";
 	const BASE_URL = "/ebargain";
+	const HOST = "localhost";
+	const DB_USERNAME = "";
+	const DB_PASSWORD = "";
 };
